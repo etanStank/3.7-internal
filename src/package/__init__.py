8ec2b0ed57ee4da5f45549a8e03da66e24c3f778
Rohan <20042@jhc.school.nz>
2026-04-21 13:00:56 +1200

    Changed file structure

--- src/package/__init__.py
+++ src/package/__init__.py
@@ -3,4 +3,16 @@
 My temporary testing file, I really need to get the test cases file working..
 Too early to deal with edge cases.
 
+TODO: 
+
 """
+
+import chapter
+import reader
+
+#chapter1 = chapter.Chapter("Chapter1", "asd", "Book1")
+
+#print(chapter1.state)
+#chapter1.setstate("Preparing")
+
+reader.init()
